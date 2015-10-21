23a5827e1d8a092ae57a9207f22a564b3de7395b
shengdzhang <shengdzhang@gmail.com>
2015-10-20 17:06:06 -0700

    Prepare to push to heroku5

--- app/assets/javascripts/components/media_edit.js.jsx
+++ app/assets/javascripts/components/media_edit.js.jsx
@@ -36,9 +36,7 @@ var MediaEdit = React.createClass ({
   },
   handleLink: function (e) {
     e.preventDefault();
-    var upload_preset = 'lg2jfbk8';
-    var cloud_name = "catstagram";
-    cloudinary.openUploadWidget({upload_preset: upload_preset, cloud_name: cloud_name}, this.handleResponse);
+    cloudinary.openUploadWidget({upload_preset: window.cloudinary_upload_preset, cloud_name: window.cloudinary_cloud_name}, this.handleResponse);
   },
   handleResponse: function (error, result) {
     if(error) {
